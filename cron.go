b898a518abda13af74565d212424a54ecb3307c9
jy00566722 <jy00566723@gmail.com>
2022-11-20 00:27:04 +0800

    增加定时任务

---
+++ cron.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jy00566722/movies/movice"
+	"github.com/robfig/cron/v3"
+)
+
+//定时任务
+func MyCron() {
+	fmt.Println("启动定时任务:....")
+	c := cron.New(cron.WithSeconds())
+	c.AddFunc("0 30 0 * * *", func() {
+		fmt.Printf("运行定时任务:%v\n", time.Now())
+		go movice.MoviceCtronGetDate()
+	})
+	c.Start()
+	fmt.Println("定时任务启动成功!....")
+}
