194cf2e5640be5b757f5c9724aa4f062835b5c69
jy00566722 <jy00566723@gmail.com>
2022-11-20 00:29:44 +0800

    a

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import "github.com/jy00566722/movies/movice"
-
 // var moviceService movice.MoviceService
 
 func main() {
-	go movice.MoviceCtronGetDate()
+	MyCron()
 	r := SetupRouter() //原框架默认路由
 	r.Run(":8080")
 
